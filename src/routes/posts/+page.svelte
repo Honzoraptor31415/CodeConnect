c5d6355560e8e2df9ff49b72e8ca970d5d9657c3
Honzoraptor <j.ackermann@email.cz>
2023-12-11 18:50:19 +0100

    Added some new styles to the /posts. Still needs to be fixed.

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -18,86 +18,48 @@
 <main class="non-header-main">
   <h2 id="about">Posts</h2>
   <div class="posts-wrp">
-    <a href="/posts/postOne" class="post-link">
-      <div class="post">
-        <h3 class="post-heading">Post name</h3>
-        <p class="post-date">10.12. 2023</p>
-        <p class="post-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          velit nam maiores, numquam pussy, enim quia suscipit sus unde
-          voluptates amogus omnis sapiente quam totam quaerat poopin' in
-          repellendus officiis?????? Lul
-        </p>
-        <div class="post-interactions">
-          <div class="reactions">
-            <button class="reaction-btn"> </button>
-            <div class="reactions-line"></div>
-            <button class="reaction-btn"> </button>
-          </div>
-          <p class="post-views">Post was seen 69 times.</p>
+    <div class="post">
+      <h3 class="post-heading">Post name</h3>
+      <p class="post-date">10.12. 2023</p>
+      <p class="post-text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        velit nam maiores, numquam pussy, enim quia suscipit sus unde voluptates
+        amogus omnis sapiente quam totam quaerat poopin' in repellendus
+        officiis?????? Lul
+      </p>
+      <div class="post-interactions">
+        <div class="reactions">
+          <button class="reaction-btn">
+            <img src="heart-icon.svg" class="reaction-icon" alt="Heart icon" />
+            <p class="reaction-counter">{Math.floor(Math.random() * 1000)}</p>
+          </button>
         </div>
-      </div>
-    </a>
-    <a href="/posts/postOne" class="post-link">
-      <div class="post">
-        <h3 class="post-heading">Post name</h3>
-        <p class="post-date">10.12. 2023</p>
-        <p class="post-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          velit nam maiores, numquam pussy, enim quia suscipit sus unde
-          voluptates amogus omnis sapiente quam totam quaerat poopin' in
-          repellendus officiis?????? Lul
+        <p class="post-views">
+          Seen {Math.floor(Math.random() * 10000)} times.
         </p>
-        <div class="post-interactions">
-          <div class="reactions">
-            <button class="reaction-btn"> </button>
-            <div class="reactions-line"></div>
-            <button class="reaction-btn"> </button>
-          </div>
-          <p class="post-views">Post was seen 69 times.</p>
-        </div>
       </div>
-    </a>
-    <a href="/posts/postOne" class="post-link">
-      <div class="post">
-        <h3 class="post-heading">Post name</h3>
-        <p class="post-date">10.12. 2023</p>
-        <p class="post-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          velit nam maiores, numquam pussy, enim quia suscipit sus unde
-          voluptates amogus omnis sapiente quam totam quaerat poopin' in
-          repellendus officiis?????? Lul
+    </div>
+    <div class="post">
+      <h3 class="post-heading">Post name</h3>
+      <p class="post-date">10.12. 2023</p>
+      <p class="post-text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        velit nam maiores, numquam pussy, enim quia suscipit sus unde voluptates
+        amogus omnis sapiente quam totam quaerat poopin' in repellendus
+        officiis?????? Lul
+      </p>
+      <div class="post-interactions">
+        <div class="reactions">
+          <button class="reaction-btn">
+            <img src="heart-icon.svg" class="reaction-icon" alt="Heart icon" />
+            <p class="reaction-counter">{Math.floor(Math.random() * 1000)}</p>
+          </button>
+        </div>
+        <p class="post-views">
+          Seen {Math.floor(Math.random() * 10000)} times.
         </p>
-        <div class="post-interactions">
-          <div class="reactions">
-            <button class="reaction-btn"></button>
-            <div class="reactions-line"></div>
-            <button class="reaction-btn"> </button>
-          </div>
-          <p class="post-views">Post was seen 69 times.</p>
-        </div>
       </div>
-    </a>
-    <a href="/posts/postOne" class="post-link">
-      <div class="post">
-        <h3 class="post-heading">Post name</h3>
-        <p class="post-date">10.12. 2023</p>
-        <p class="post-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          velit nam maiores, numquam pussy, enim quia suscipit sus unde
-          voluptates amogus omnis sapiente quam totam quaerat poopin' in
-          repellendus officiis?????? Lul
-        </p>
-        <div class="post-interactions">
-          <div class="reactions">
-            <button class="reaction-btn"> </button>
-            <div class="reactions-line"></div>
-            <button class="reaction-btn"> </button>
-          </div>
-          <p class="post-views">Post was seen 69 times.</p>
-        </div>
-      </div>
-    </a>
+    </div>
   </div>
 </main>
 <Footer />
