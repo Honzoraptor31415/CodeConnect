c6c76a2706f86188e256e923331d560e73766eb3
Honzoraptor <j.ackermann@email.cz>
2023-12-13 11:11:53 +0100

    Added an error page and made some more styles for /posts

---
+++ src/routes/+error.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<body>
+  <main>
+    <h1>Error: {$page.error.message}</h1>
+    <h2>{$page.status}</h2>
+  </main>
+</body>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    min-height: 100vh;
+    display: grid;
+    place-items: center;
+    background: rgb(40, 40, 40);
+    margin: 0;
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      "Open Sans",
+      "Helvetica Neue",
+      sans-serif;
+    color: rgba(255, 255, 255, 0.736);
+  }
+  h1,
+  h2 {
+    font-weight: 100;
+  }
+  h2 {
+    color: rgba(255, 255, 255, 0.411);
+  }
+  main {
+    text-align: center;
+  }
+</style>
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -28,92 +28,48 @@
         officiis?????? Lul
       </p>
       <div class="post-interactions">
-        <div class="reactions">
-          <button
-            on:click={() => {
-              if (
-                document
-                  .getElementById("p1")
-                  .classList.contains("liked-reaction-btn")
-              ) {
-                document
-                  .getElementById("p1")
-                  .classList.remove("liked-reaction-btn");
-                document.getElementById("p1t").innerText =
-                  parseInt(document.getElementById("p1t").innerText) - 1;
-              } else {
-                document
-                  .getElementById("p1")
-                  .classList.add("liked-reaction-btn");
-                document.getElementById("p1t").innerText =
-                  parseInt(document.getElementById("p1t").innerText) + 1;
-              }
-            }}
-            id="p1"
-            class="reaction-btn"
-          >
-            <img
-              src="heart-icon.svg"
-              class="reaction-icon no-user-drag"
-              alt="Heart icon"
-            />
-            <p id="p1t" class="reaction-counter">
-              {Math.floor(Math.random() * 1000)}
-            </p>
-          </button>
+        <div class="post-ints-left">
+          <div class="reactions">
+            <button
+              on:click={() => {
+                if (
+                  document
+                    .getElementById("p1")
+                    .classList.contains("liked-reaction-btn")
+                ) {
+                  document
+                    .getElementById("p1")
+                    .classList.remove("liked-reaction-btn");
+                  document.getElementById("p1t").innerText =
+                    parseInt(document.getElementById("p1t").innerText) - 1;
+                } else {
+                  document
+                    .getElementById("p1")
+                    .classList.add("liked-reaction-btn");
+                  document.getElementById("p1t").innerText =
+                    parseInt(document.getElementById("p1t").innerText) + 1;
+                }
+              }}
+              id="p1"
+              class="reaction-btn"
+            >
+              <img
+                src="heart-icon.svg"
+                class="reaction-icon no-user-drag"
+                alt="Heart icon"
+              />
+              <p id="p1t" class="reaction-counter">
+                {Math.floor(Math.random() * 1000)}
+              </p>
+            </button>
+          </div>
+          <p class="post-views">
+            Seen {Math.floor(Math.random() * 10000)} times.
+          </p>
         </div>
-        <p class="post-views">
-          Seen {Math.floor(Math.random() * 10000)} times.
-        </p>
-      </div>
-    </div>
-    <div class="post">
-      <h3 class="post-heading">Post name</h3>
-      <p class="post-date">10.12. 2023</p>
-      <p class="post-text">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-        velit nam maiores, numquam pussy, enim quia suscipit sus unde voluptates
-        amogus omnis sapiente quam totam quaerat poopin' in repellendus
-        officiis?????? Lul
-      </p>
-      <div class="post-interactions">
-        <div class="reactions">
-          <button
-            on:click={() => {
-              if (
-                document
-                  .getElementById("p2")
-                  .classList.contains("liked-reaction-btn")
-              ) {
-                document
-                  .getElementById("p2")
-                  .classList.remove("liked-reaction-btn");
-                document.getElementById("p2t").innerText =
-                  parseInt(document.getElementById("p2t").innerText) - 1;
-              } else {
-                document
-                  .getElementById("p2")
-                  .classList.add("liked-reaction-btn");
-                document.getElementById("p2t").innerText =
-                  parseInt(document.getElementById("p2t").innerText) + 1;
-              }
-            }}
-            id="p2"
-            class="reaction-btn"
-          >
-            <img
-              src="heart-icon.svg"
-              class="reaction-icon no-user-drag"
-              alt="Heart icon"
-            />
-            <p id="p2t" class="reaction-counter">
-              {Math.floor(Math.random() * 1000)}
-            </p>
-          </button>
+        <div class="post-ints-right">
+          <a href="/posts/onePost" class="view-post-link">View post →</a>
         </div>
-        <p class="post-views">
-          Seen {Math.floor(Math.random() * 10000)} times.
-        </p>
       </div>
     </div>
   </div>
