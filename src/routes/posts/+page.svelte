84162b3bbda12a1ce4db56bb13c848d6db93590b
Honzoraptor <j.ackermann@email.cz>
2023-12-16 16:16:37 +0100

    Bug with duplicating posts fixed!

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -5,6 +5,7 @@
   import GandalfLogo from "$lib/images/gandalf-macbook.jpg";
   import "../styles.css";
   import { browser } from "$app/environment";
+  let loaded = false;
   if (browser) {
     document.addEventListener(
       "contextmenu",
@@ -28,9 +29,8 @@
     appId: "1:747780271550:web:f6680e4af9f41f4d4cddef",
   });
 
-  let pages = [];
-
   async function loadData() {
+    let pages = [];
     const dbRef = ref(getDatabase());
     const data = await get(child(dbRef, `cc/pages`))
       .then((snapshot) => {
@@ -54,83 +54,81 @@
 <LoggedDevNav />
 <main class="non-header-main">
   <h2>Posts</h2>
-  <div class="posts-wrp">
+  <div id="post-wrp" class="posts-wrp">
     {#await loadData() then dataPages}
-      {#if browser && document.getElementsByClassName("post").length < 1}
-        {#each dataPages as page}
-          <div class="post">
-            <h3 class="post-heading">{page.name}</h3>
-            <div class="post-top-info">
-              <div class="post-pfp-wrp">
-                <img src={GandalfLogo} alt="User's pfp" class="post-user-pfp" />
-              </div>
-              <p class="post-date">{page.date}</p>
+      {#each dataPages as page}
+        <div class="post">
+          <h3 class="post-heading">{page.name}</h3>
+          <div class="post-top-info">
+            <div class="post-pfp-wrp">
+              <img src={GandalfLogo} alt="User's pfp" class="post-user-pfp" />
             </div>
-            <p class="post-text">
-              {#if page.text.length <= 500}
-                {page.text}
-              {:else}
-                {@html `${page.text.slice(
-                  0,
-                  496,
-                )} <span class="post-mtc">...</span>`}
-              {/if}
-            </p>
-            <div class="post-interactions">
-              <div class="post-ints-left">
-                <div class="reactions">
-                  <button
-                    id={`p${page.path}`}
-                    on:click={() => {
-                      if (
-                        document
-                          .getElementById(`p${page.path}`)
-                          .classList.contains("liked-reaction-btn")
-                      ) {
-                        document
-                          .getElementById(`p${page.path}`)
-                          .classList.remove("liked-reaction-btn");
-                        document.getElementById(`p${page.path}t`).innerText =
-                          parseInt(
-                            document.getElementById(`p${page.path}t`).innerText,
-                          ) - 1;
-                      } else {
-                        document
-                          .getElementById(`p${page.path}`)
-                          .classList.add("liked-reaction-btn");
-                        document.getElementById(`p${page.path}t`).innerText =
-                          parseInt(
-                            document.getElementById(`p${page.path}t`).innerText,
-                          ) + 1;
-                      }
-                    }}
-                    class="reaction-btn"
-                  >
-                    <img
-                      src="heart-icon.svg"
-                      class="reaction-icon no-user-drag"
-                      alt="Heart icon"
-                    />
-                    <p id={`p${page.path}t`} class="reaction-counter">
-                      {page.likes}
-                    </p>
-                  </button>
-                </div>
-                <p class="post-views">
-                  Seen {page.views}x
-                </p>
-              </div>
-              <div class="post-ints-right">
-                <a href={`posts/${page.path}`} class="view-post-link"
-                  >View post →</a
+            <p class="post-date">{page.date}</p>
+          </div>
+          <p class="post-text">
+            {#if page.text.length <= 500}
+              {page.text}
+            {:else}
+              {@html `${page.text.slice(
+                0,
+                496,
+              )} <span class="post-mtc">...</span>`}
+            {/if}
+          </p>
+          <div class="post-interactions">
+            <div class="post-ints-left">
+              <div class="reactions">
+                <button
+                  id={`p${page.path}`}
+                  on:click={() => {
+                    if (
+                      document
+                        .getElementById(`p${page.path}`)
+                        .classList.contains("liked-reaction-btn")
+                    ) {
+                      document
+                        .getElementById(`p${page.path}`)
+                        .classList.remove("liked-reaction-btn");
+                      document.getElementById(`p${page.path}t`).innerText =
+                        parseInt(
+                          document.getElementById(`p${page.path}t`).innerText,
+                        ) - 1;
+                    } else {
+                      document
+                        .getElementById(`p${page.path}`)
+                        .classList.add("liked-reaction-btn");
+                      document.getElementById(`p${page.path}t`).innerText =
+                        parseInt(
+                          document.getElementById(`p${page.path}t`).innerText,
+                        ) + 1;
+                    }
+                  }}
+                  class="reaction-btn"
                 >
+                  <img
+                    src="heart-icon.svg"
+                    class="reaction-icon no-user-drag"
+                    alt="Heart icon"
+                  />
+                  <p id={`p${page.path}t`} class="reaction-counter">
+                    {page.likes}
+                  </p>
+                </button>
               </div>
+              <p class="post-views">
+                Seen {page.views}x
+              </p>
+            </div>
+            <div class="post-ints-right">
+              <a href={`posts/${page.path}`} class="view-post-link"
+                >View post →</a
+              >
             </div>
           </div>
-        {:else}
-          <h4>No data available</h4>
-        {/each}
-      {/if}
+        </div>
+      {:else}
+        <h4>No data available</h4>
+      {/each}
     {/await}
   </div>
 </main>
